4476399212f4d34710950f05348df59e63f506da
Matthew Feng <mattfeng@mit.edu>
2018-07-28 03:55:39 +0300

    Add push and pull from server

--- solution/js/todo.js
+++ solution/js/todo.js
@@ -1,3 +1,4 @@
+/* START TODO2 SOLUTION */
 function toggleComplete(obj) {
   $(obj).toggleClass("checked");
 }
@@ -31,4 +32,76 @@ function createTodoItem() {
 
 function deleteTodoItem(obj) {
   $(obj).parents("li")[0].remove();
+}
+
+/* END TODO2 SOLUTION */
+
+function readTodoItems() {
+  var todoInfo = [];
+
+  $("li").each(function() {
+    var info = {};
+    info.complete = $(this).find(".todo-item-checkbox").first().hasClass("checked");
+    info.task = $(this).find("input[name='todo-item-text']").val();
+    todoInfo.push(info);
+  });
+
+  return JSON.stringify(todoInfo);
+}
+
+function readListName() {
+  return $("#todo-list-name").val();
+}
+
+function readPassword() {
+  return $("#todo-list-password").val();
+}
+
+function showMessage() {
+  $("#message").removeClass("invisible");
+
+  setTimeout(function () {
+    $("#message").addClass("invisible");
+  }, 2000);
+
+}
+
+function pullTodoList() {
+  var list = readListName();
+  var password = readPassword();
+
+  $.ajax("http://localhost:3000/todo", {
+    method: "POST",
+    data: {
+      list: list,
+      password: password,
+    },
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+}
+
+function pushTodoList() {
+  var list = readListName();
+  var password = readPassword();
+  var tasks = readTodoItems();
+
+  $.ajax("http://localhost:3000/todo/edit", {
+    method: "POST",
+    data: {
+      list: list,
+      password: password,
+      tasks: tasks
+    },
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
 }
